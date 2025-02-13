0d075570682ccca184f8320c1ffc626795090725
Mason Scott <masonscott2112@gmail.com>
2025-02-12 21:02:00 -0700

    JS1 - refactored styles for login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './login.css'
+import '../custom-form.css'
 
 export function Login() {
   return (
-    <main className="">
-      <div>Login displayed here</div>
+    <main className="login-page">
+      <h1>Login To Play</h1>
+      <div className='login-form'>
+        <div className="custom-form">
+          <span>Email:</span>
+          <input type="text" placeholder="your@email.com" />
+        </div>
+
+        <div className="custom-form">
+          <span>Password:</span>
+          <input type="password" placeholder="password" />
+        </div>
+        <button type="submit">Login</button>
+        <button type="submit">Create</button>
+      </div>
     </main>
   );
 }
