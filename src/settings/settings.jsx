b7da15be55ad3a12a5126fce7920abeb63852818
Mason Scott <masonscott2112@gmail.com>
2025-02-12 21:32:18 -0700

    JS1 - refactored styles for settings page

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './settings.css'
 
 export function Settings() {
   return (
-    <main className="">
-      <div>Settings displayed here</div>
+    <main className="settings-page">
+      <div>
+        <h1>User Info</h1>
+        <li><strong>Username:</strong> ladiesman217</li>
+        <li><strong>Email:</strong> ladiesman217@gmail.com</li>
+        <li><strong>Password:</strong> ******* <button>show</button></li>
+      </div>
     </main>
   );
 }
