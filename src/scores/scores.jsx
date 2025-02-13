2f4b98cedc6d74961b9b0aa5810e26971739d72f
Mason Scott <masonscott2112@gmail.com>
2025-02-12 21:26:23 -0700

    JS1 - refactored styles for scores page - also removed the bootstrap styling becuase it was being annoying and overriding stuff.

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import './scores.css'
+
+
 
 export function Scores() {
   return (
-    <main className="">
-      <div>Scores displayed here</div>
+    <main className="scores-page">
+      <h1>Leaderboard</h1>
+      
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Abram</td>
+            <td>2:43</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Beth</td>
+            <td>4:21</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Chrysanthemum</td>
+            <td>5:08</td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
