79fb130e3ff3b60b0f9cd3282165feaae995e16c
Mason Scott <masonscott2112@gmail.com>
2025-02-25 20:43:59 -0700

    JS2 - added ability to view username and password on settings page as well as added reactivity to the 'show password' button

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './settings.css'
 
-export function Settings() {
+export function Settings({userName, password}) {
+
+  const [isVisible, setIsVisible] = React.useState(false)
+
   return (
     <main className="settings-page">
       <div>
         <h1>User Info</h1>
-        <li><strong>Username:</strong> ladiesman217</li>
-        <li><strong>Password:</strong> ******* <button>show</button></li>
+        <li><strong>Username:</strong> {userName} </li>
+        <li>
+          <button 
+            onMouseDown={()=>setIsVisible(true)}
+            onMouseUp={()=>setIsVisible(false)}
+            onMouseLeave={()=>setIsVisible(false)}
+            onTouchStart={() => setIsVisible(true)}  /* for mobile */
+            onTouchEnd={() => setIsVisible(false)}   /* for mobile */
+          >
+            show
+          </button>
+          <strong>Password: </strong> 
+          <span className='password-text'> {isVisible ? password : '********' } </span>
+        </li>
       </div>
     </main>
   );
